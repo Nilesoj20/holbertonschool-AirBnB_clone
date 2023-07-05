d10d237cd33082fea0dc5a0b9036d35dcd8d4d74
Nilesoj20 <jvrn2014@gmail.com>
2023-07-05 10:32:08 -0700

    fix: base model file

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import uuid
+import datetime
 
 
 class BaseModel:
     """ Comentario """
 
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self, created_at, updated_at):
         """ Comentario """
         self.id = uuid.uuid4()
         self.created_at = created_at
@@ -13,5 +14,13 @@ class BaseModel:
 
     def __str__(self):
         """ Comentario """
-        print(f"")
+        print(f"[{__class__.__name__}] (self.id) self.__dict__")
+
+    def save(self):
+        """ Comentario """
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """ Comentario """
+        return {'}
 
