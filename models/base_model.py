844b76c14efffadbb2671fdc192c2b158e2c3157
Nilesoj20 <jvrn2014@gmail.com>
2023-07-05 14:36:13 -0700

    fix: creating the to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -4,23 +4,32 @@ import datetime
 
 
 class BaseModel:
-    """ Comentario """
+    """defines all common attributes/methods for other classes """
 
-    def __init__(self, created_at, updated_at):
-        """ Comentario """
-        self.id = uuid.uuid4()
-        self.created_at = created_at
-        self.updated_at = updated_at
+    def __init__(self):
+        """ init constructor method """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        """ Comentario """
-        print(f"[{__class__.__name__}] (self.id) self.__dict__")
+        """ Method __str__ string representation of an object"""
+        class_name = self.__class__.__name__
+        return (f"[{class_name}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        """ Comentario """
+        """ Public instance methods save updates updated_at """
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """ Comentario """
-        return {'}
-
+        """ dictionary representation with “simple object type” """
+        value_class = self.__class__.__name__
+        value_update = self.updated_at.isoformat()
+        value_created = self.created_at.isoformat()
+        dic = dict()
+        for clave, value in self.__dict__.items():
+            dic[clave] = value
+            dic['update_at'] = value_update
+            dic['created_at'] = value_created
+            dic['__class__'] = value_class
+        return dic
