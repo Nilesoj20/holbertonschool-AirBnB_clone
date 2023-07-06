6fe355afc57003b12cc9dbb311f5a505a14696f8
Stefano-BML <stefanoml1128@gmail.com>
2023-07-06 00:41:07 -0700

    other idea

--- models/base_model.py
+++ models/base_model.py
@@ -1,47 +1,44 @@
 #!/usr/bin/python3
+"""Model Base """
 import uuid
+import models
 from datetime import datetime
 
 
 class BaseModel:
-    """defines all common attributes/methods for other classes """
-
+    """class Base"""
     def __init__(self, *args, **kwargs):
-        """ init constructor method """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        f = "%Y-%m-%d %H:%M:%S.%f"
-        if len(kwargs) != 0:
-            for clave, valor in kwargs.items():
-                if clave == "updated_at":
-                    self.__dict__[clave] = datetime.strptime(valor, f)
-                    continue
-                elif clave == "created_at":
-                    self.__dict__[clave] = datetime.strptime(valor, f)
-                    continue
-                else:
-                    self.__dict__[clave] = valor
+        """ Constructor """
+        joselin = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    value = datetime.strptime(value, joselin)
+                if key == "updated_at":
+                    value = datetime.strptime(value, joselin)
+                if key != "__class__":
+                    setattr(self, key, value)
         else:
+            self.id = str(uuid.uuid4())  # unique id
+            self.created_at = datetime.now()  # datetime when is created
+            self.updated_at = datetime.now()  # date when is updated
             models.storage.new(self)
 
     def __str__(self):
-        """ Method __str__ string representation of an object"""
-        class_name = self.__class__.__name__
-        return (f"[{class_name}] ({self.id}) {self.__dict__}")
+        """ print() __str__ method """
+        """" For pep8 validation"""
+        className = self.__class__.__name__
+        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
 
     def save(self):
-        """ Public instance methods save updates updated_at """
+        """ updates with the current datetime """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """ dictionary representation with “simple object type” """
-        value_class = self.__class__.__name__
-        value_update = self.updated_at.strftime('%Y-%m-%d %H:%M:%S.%f')
-        value_created = self.created_at.strftime('%Y-%m-%d %H:%M:%S.%f')
-        dic = self.__dict__.copy()
-        dic["updated_at"] = value_update
-        dic["created_at"] = value_created
-        dic["__class__"] = value_class
-        return dic
+        '''returns a dictionary with all keys/value of the instance'''
+        dict_copy = self.__dict__.copy()
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        dict_copy['__class__'] = self.__class__.__name__
+        return dict_copy
