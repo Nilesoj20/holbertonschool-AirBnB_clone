f1f52e0d81aaf38ca11c0cc5c68fc14c147c2ef4
Stefano-BML <stefanoml1128@gmail.com>
2023-07-07 12:07:36 -0700

    console

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/bin/bash/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """ exit the program """
+        return True
+    def do_EOF(self, arg):
+        """ exit with ctrl + D """
+        return True
+    def emptyline(self):
+        """ an empty line is entered (do nothing) """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
