f74d74ea7ab7e2c1c58244b68af6c3f0f3e42b1d
Stefano-BML <stefanoml1128@gmail.com>
2023-07-07 15:09:23 -0700

    add base model

--- models/base_model.py
+++ models/base_model.py
@@ -24,6 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Return a string representation of the BaseModel instance."""
@@ -34,7 +35,7 @@ class BaseModel:
         """Update the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
         models.storage.save()
-    
+        
     def to_dict(self):
         """Return a dictionary representation of the BaseModel instance."""
         dict_copy = self.__dict__.copy()
