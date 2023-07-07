498053603bb75ad26735f69bff38589f5b63a216
Stefano-BML <stefanoml1128@gmail.com>
2023-07-07 12:16:47 -0700

    fix pycodestyle console

--- console.py
+++ console.py
@@ -1,18 +1,25 @@
 #!/bin/bash/python3
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
+    """ command of my interactive shell
+    args:
+        promt(str): started prompt request"""
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
         """ exit the program """
         return True
+
     def do_EOF(self, arg):
         """ exit with ctrl + D """
         return True
+
     def emptyline(self):
         """ an empty line is entered (do nothing) """
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
