01d41310df033cb10a878407fbf889151034e71b
Stefano-BML <stefanoml1128@gmail.com>
2023-07-09 12:31:14 -0700

    feat: another version of test amenity :rocket:

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,43 +2,37 @@
 """Unittest module for the Amenity Class."""
 
 import unittest
-from datetime import datetime
-import time
+import models
 from models.amenity import Amenity
-import re
-import json
-from models.engine.file_storage import FileStorage
-import os
-from models import storage
-from models.base_model import BaseModel
+
 
 class TestAmenity(unittest.TestCase):
-
-    """Test Cases for the Amenity class."""
-
     def setUp(self):
-        """Sets up test methods."""
-        pass
+        self.amenity = Amenity()
 
-    def tearDown(self):
-        """Tears down test methods."""
-        self.resetStorage()
-        pass
+    def test_attributes(self):
+        self.assertTrue(hasattr(models.Amenity, 'name'))
+        self.assertEqual(models.Amenity.name, '')
 
-    def resetStorage(self):
-        """Resets FileStorage data."""
-        FileStorage._FileStorage__objects = {}
-        if os.path.isfile(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
+    def test_inheritance(self):
+        self.assertIsInstance(self.amenity, models.BaseModel)
 
-    def test_8_instantiation(self):
-        """Tests instantiation of Amenity class."""
+    def test_str(self):
+        string = str(self.amenity)
+        self.assertIsInstance(string, str)
 
-        b = Amenity()
-        self.assertEqual(str(type(b)), "<class 'models.amenity.Amenity'>")
-        self.assertIsInstance(b, Amenity)
-        self.assertTrue(issubclass(type(b), BaseModel))
+    def test_save(self):
+        self.amenity.save()
+        self.assertIsNotNone(self.amenity.updated_at)
+
+    def test_to_dict(self):
+        amenity_dict = self.amenity.to_dict()
+        self.assertIsInstance(amenity_dict, dict)
+        self.assertEqual(amenity_dict['__class__'], 'Amenity')
+        self.assertIsNotNone(amenity_dict['created_at'])
+        self.assertIsNotNone(amenity_dict['updated_at'])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
+
